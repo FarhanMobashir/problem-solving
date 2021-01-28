e9b6eea739040937bcec9eeeeaf2c3fb6a86601a
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-01-28 21:20:47 +0530

    solves romanNumeralConverter problem

---
+++ problems/generatingWordsFromNames.js
@@ -0,0 +1,11 @@
+/*
+
+? Write a function that returns true if a given name can generate an array of words.
+* Input : "Justin Bieber", ["injures", "ebb", "it"] -> Output : true
+* Input : "Jeff Goldblum", ["jog", "meld", "bluffs"] -> Output : false
+
+todo Coming Up With The Logic
+
+? --> Breaking the problem
+-
+*/
--- problems/vowelFamilies.js
+++ problems/vowelFamilies.js
@@ -15,22 +15,30 @@ todo 1. Coming Up with Logic
 */
 
 function vowelFamilies(arr) {
+    // let allvowel = ['a', 'i', 'u'];
     let vowel = [];
+
     let result = [];
     for (let j = 0; j < arr[0].length; j++) {
+
         if (arr[0][j] === 'a' || arr[0][j] === 'e' || arr[0][j] === 'i' || arr[0][j] === 'o' || arr[0][j] === 'u') {
             vowel.push(arr[0][j]);
         }
     }
+    let set = new Set(vowel)
+    vowel = [...set]
     vowel.forEach(function (letter) {
-        arr.forEach(function (item) {
-            if ((item.includes(letter))) {
-                result.push(item)
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].includes(letter)) {
+                arr.splice(1, i)
             }
-        })
+        }
     })
     console.log(vowel);
-    console.log(result);
+    // let finalResult = new Set(result);
+    // finalResult = result
+    // console.log([...finalResult]);
+    return arr;
 }
 
-console.log(vowelFamilies(["many", "carriage", "emit", "apricot", "animal"]));
+console.log(vowelFamilies(["hoops", "chuff", "bot", "bottom"]));
